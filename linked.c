9e461fb2e1a4dc95df4c37b36b694f09fa9b456a
SOORAJ KUMAR <121786021+ITSSOORAJ@users.noreply.github.com>
2023-12-12 01:23:44 +0530

    Update linked.c

--- linked.c
+++ linked.c
@@ -1,94 +1,89 @@
-#include <stdio.h>
-//#include <conio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 struct file
 {
-    int si; // The Starting Index of the file.
-    int len; // Total length of the file.
-    int b[100]; // The blocks in the file.
-    struct node *link; 
-} f[100];
+int size;
+int start;
+struct node*link;
+}f[100];
 
-int mem[100], n;
+
 struct node
 {
-    int n;
-    struct node *next;
+int bn;
+struct node* next;
 };
 
 void main()
 {
-    //Marks all positions as unoccupied.
-    for (int i = 0; i < 100; i++)
-    {
-        mem[i] = 0;
-    }
+int i,j,bl,occu[100],n;
+for(i=0;i<100;i++)
+	{
+ 	  occu[i]=0;
+	}
+printf("ENTER THE NO OF FILES \n");
+scanf("%d",&n);
+for(i=0;i<n;)
+	{
+	printf("FILE%d\n",i+1);
+	printf("ENTER THE STARTING BLOCK \n");
+	scanf("%d",&f[i].start);
+	if(occu[f[i].start]==1)
+		{
+		 printf("CANNOT BE ALLOCATED \n");
+		 continue;
+		}
+	 else
+		{
+		printf("ENTER THE NO OF BLOCKS \n");
+		 scanf("%d",&f[i].size);
+		 occu[f[i].start]=1;
+		printf("ENTER THE BLOCKS OF FILE \n");
+		     for(j=0;j<f[i].size;j++)
+			{
+			 x:scanf("%d",&bl);
+			  if(occu[bl]==0)
+				{
+					struct node * ll= (struct node*)malloc(sizeof(struct node));
+					   ll->bn=bl;
+					   ll->next=NULL;
 
-    printf("\nEnter the no: of Files : ");
-    scanf("%i", &n);
-    for (int i = 0; i < n;)
+					if(f[i].link==NULL)
+							{	
+							   f[i].link=ll;//sincef[i]is an array rather -> we use . and used for distinguish between files
+							}
+					 else
+							{
+							  struct  node * temp=f[i].link;
+							 	while(temp->next!=NULL)
+									{
+										 temp=temp->next;
+									}
+								 temp->next=ll;
+							}
+					occu[bl]=1;
+				}
+				else
+				{
+				 printf("BLOCK ALREADY ALLOCATED TRY ANOTHER \n");
+				  goto x;		
+				}
+			}
+		}
+      i++;
+	}
+  printf("ALLOCATION DETAILS \n");
+    printf("FILE\tSTART\tSIZE\tBLOCKS\n");
+    for ( i = 0; i < n; i++)
     {
-        printf("\nfile%i", i + 1);
-        printf("\nEnter the Starting block : ");
-        scanf("%i", &f[i].si);
-        if (mem[f[i].si] == 1)
-        {
-            printf("\nFile%i Cannot be Allocated\n", i + 1);
-            continue;
-        }
-        else
-        {
-            printf("\nEnter the no: BLOCKS : ");
-            scanf("%i", &f[i].len);
-            int check = 0;
- int kk;
-            mem[f[i].si] = 1;
-	   printf("ENTER THE BLOCKS\n");
-            for (int j = 0; j < f[i].len; j++)
-            {
-               // for (int kk = 1; kk < 100; kk++)
-                //{ 
-		   x:scanf("%d",&kk);
-                    if (mem[kk] == 0)
-                    {
-                        struct node *ll = (struct node *)malloc(sizeof(struct node));
-                        ll->n = kk;
-                        ll->next = NULL;
-                        if (f[i].link == NULL)
-                            f[i].link = ll;
-                        else
-                        {
-                            struct node *temp = f[i].link;
-                            while (temp->next != NULL)
-                            {
-                                temp = temp->next;
-                            }
-                            temp->next = ll;
-                        }
-                        mem[kk] = 1;
-                        //break;
-                    //}
-                }
-		else
-		  {
-		   printf("BLOCK ALREADY OCCUPIED ENTER ANOTHER BLOCK\n");
-		    goto x;
-		  }
-            }
-        }
-        i++;
-    }
-  printf("FILE\tSTART\tSIZE\tBLOCKS\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d\t%d\t%d", i + 1, f[i].si,f[i].len);
+        printf("%d\t%d\t%d", i + 1, f[i].start,f[i].size);
         struct node *temp = f[i].link;
         while (temp != NULL)
         {
-            printf("\t%i\t->", temp->n);
+            printf("\t%i\t->", temp->bn);
             temp = temp->next;
         }
 	printf("\n");
     }
 }
+
